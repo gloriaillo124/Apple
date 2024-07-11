7f32b43d423c513fb2a0aa57ce3a39162f1e3a50
gloriaillo124 <gloriaillo124@gmail.com>
2024-07-12 01:03:31 +0200

    second commit

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function slider(anything){
+  document.querySelector('.phone').src = anything;
+}
+
+function couleur(color){
+  const sec = document.querySelector('.global');
+  sec.style.background = color;
+}
